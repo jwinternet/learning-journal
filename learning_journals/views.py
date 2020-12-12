cf16653dfaeccc1b1cd7aac8b9e46d292014670d
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-12-11 19:53:41 -0500

    Created the topics page.

--- learning_journals/views.py
+++ learning_journals/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 
 def index(request):
 	"""The home page for Learning Journal."""
 	return render(request, 'learning_journals/index.html')
+
+
+def topics(request):
+	"""show all topics."""
+	topics = Topic.objects.order_by('date_added')
+	context = {'topics': topics}
+	return render(request, 'learning_journals/topics.html', context)
