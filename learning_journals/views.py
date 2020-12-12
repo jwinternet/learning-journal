bf4047a2242d521c3128aded8c859dbcbe2f71af
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-12-11 21:31:34 -0500

    Completed the initial setup.

--- learning_journals/views.py
+++ learning_journals/views.py
@@ -14,4 +14,10 @@ def topics(request):
 	context = {'topics': topics}
 	return render(request, 'learning_journals/topics.html', context)
 
-##
+
+def topic(request, topic_id):
+	"""Show a single topic and all its entries."""
+	topic = Topic.objects.get(id=topic_id)
+	entries = topic.entry_set.order_by('-date_added')
+	context = {'topic': topic, 'entries': entries}
+	return render(request, 'learning_journals/topic.html', context)
