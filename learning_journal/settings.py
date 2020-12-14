86be4282137da92c1a6c9edd446afa7594304ccb
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-12-14 01:12:12 -0500

    Applied a template to multiple files and got this ready for Heroku.

--- learning_journal/settings.py
+++ learning_journal/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'learning_journals',
     'users',
 
+    # Third party apps.
+    'bootstrap4',
+
     # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
@@ -123,3 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# My settings
+LOGIN_URL = 'users:login'
+
+# Heroku settings.
+import django_heroku
+django_heroku.settings(locals())
